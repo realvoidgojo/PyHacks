e8b7262fc49143e40d516a979f33453e36ec78ab
Harish Sivaraman <dir.harishsivaraman@gmail.com>
2024-02-20 22:03:42 +0530

    intial done : netwok scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -5,6 +5,6 @@ def scan(ip):
     src_destination = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = src_destination/arp_request
     (answered , unanswered) = scapy.srp(arp_request_broadcast , timeout=1)
-    print(answered.summary())
+    print(unanswered.summary())
 
 scan("192.168.43.1/24")
